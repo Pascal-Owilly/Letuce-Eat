33c568fb539f076565640a7e1d3e308af2780b0c
Owilly <pasaclouma54@gmail.com>
2021-11-29 18:28:45 +0300

    Add js interactivity

---
+++ script.js
@@ -0,0 +1,25 @@
+// alert("Hello Pascal");
+// prompt("What is your name?");
+// document.write("Time to learn JavaScript");
+
+// document.getElementById("h2-article").innerHTML="Learn JavaScript";
+
+// var nm = prompt("Who are you?");
+// document.write(nm);
+// console.log(nm);
+// alert(nm);
+
+
+// var name;
+// name = prompt("What is your name");
+// document.write(name);
+// document.write("<h1>" + name + "</h1>");
+// document.write(document.baseURI + "<br>");
+// document.write(document.title + "<br>");
+// document.write(window.location);
+
+
+function message() {
+
+}
+message();
